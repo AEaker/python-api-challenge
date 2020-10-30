899a0735ca5ff1fd3f4d2e621c3d088b08420706
Aaron Eaker <aaroneaker@hotmail.com>
2020-10-29 22:56:23 -0400

    just updating

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "722bbdb242568c36c49f9e33db2527ce"
+
+# Google API Key
+g_key = "AIzaSyDNUbPEn0nd6M8U-bOPAekLsPpV2eWuLAk"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "c2e2cef9376b453543ec2874b7663f75"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
